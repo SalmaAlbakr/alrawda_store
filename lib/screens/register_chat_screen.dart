ed9b31d4d9c18090e0056858a7b8b0db7a51ec97
SalmaAlbakr <salalbakr19988@gmail.com>
2023-05-23 02:27:53 +0300

    add drawer

--- lib/screens/register_chat_screen.dart
+++ lib/screens/register_chat_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:alrawda_store/my_color.dart';
 import 'package:alrawda_store/screens/sign_in_chat_screen.dart';
 import 'package:flutter/material.dart';
@@ -19,156 +18,175 @@ class _RegisterScreenState extends State<RegisterScreen> {
   late String email;
   late String password;
   bool isObscure = true;
-
+  GlobalKey<FormState> formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
-      child: Scaffold(
-        body: SafeArea(
-          child: ModalProgressHUD(
-            inAsyncCall: _saving,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Image.asset(
-                    //   "image/img_1.png",
-                    //   width: 200,
-                    //   height: 200,
-                    // ),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    Text("شركة الروضه",
-                      style: GoogleFonts.alexandria(
-                        color: Colors.blue[800],
-                          fontSize: 40
-                      ),),
-                      SizedBox(height: 20,),
-                      //TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.blue[800]),),
-                    Container(
-                      width: 350,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.blue
-                        ),
+      child: Form(
+        key: formKey,
+        child: Scaffold(
+          body: SafeArea(
+            child: ModalProgressHUD(
+              inAsyncCall: _saving,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Image.asset(
+                      //   "image/img_1.png",
+                      //   width: 200,
+                      //   height: 200,
+                      // ),
+                      SizedBox(
+                        height: 25,
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: TextFormField(
-                          validator: (String ? value ) {
-                            if (value! .isEmpty || !(value.contains("@ / .com")) ) {
-                              return " من فضلك أدخل الحساب و يجب ان يحتوي علي @ / com. ";
-                            }
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                          onChanged: (value) {
-                            email = value;
-                          },
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "اكتب اسم الحساب الجديد",
+                      Text(
+                        "شركة الروضه",
+                        style: GoogleFonts.alexandria(
+                            color: Colors.blue[800], fontSize: 40),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      //TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.blue[800]),),
+                      Container(
+                        width: 350,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: TextFormField(
+                            validator: (String? value) {
+                              if (value!.isEmpty ||
+                                  !(value.contains("@ / .com"))) {
+                                return " من فضلك أدخل الحساب و يجب ان يحتوي علي @ / com. ";
+                              }
+                              return null;
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                            onChanged: (value) {
+                              email = value;
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "اكتب اسم الحساب الجديد",
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    Container(
-                      width: 350,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.blue
-                        ),
+                      SizedBox(
+                        height: 25,
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TextFormField(
-                                validator: (String ? value ) {
-                                  if (value! .isEmpty || !(value.contains("0123456789")) ) {
-                                    return "  كلمة المرور يجب ان تحتوي علي رقم ";
-                                  }
-                                },
-                                keyboardType: TextInputType.visiblePassword,
-                                obscureText: isObscure,
-                                onChanged: (value) {
-                                  password = value;
-                                },
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: " كلمة المرور",
+                      Container(
+                        width: 350,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TextFormField(
+                                  validator: (String? value) {
+                                    if (value!.isEmpty ||
+                                        !(value.contains("0123456789"))) {
+                                      return "  كلمة المرور يجب ان تحتوي علي رقم ولا تقل عن 6 حروف ";
+                                    }
+                                    return null;
+                                  },
+                                  keyboardType: TextInputType.visiblePassword,
+                                  obscureText: isObscure,
+                                  onChanged: (value) {
+                                    password = value;
+                                  },
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: " كلمة المرور",
+                                  ),
                                 ),
                               ),
-                            ),
-                            isObscure
-                                ? IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        isObscure = false;
-                                      });
-                                    },
-                                    icon: Icon(
-                                      Icons.visibility_off_outlined,color: Colors.blue,
+                              isObscure
+                                  ? IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          isObscure = false;
+                                        });
+                                      },
+                                      icon: Icon(
+                                        Icons.visibility_off_outlined,
+                                        color: Colors.blue,
+                                      ),
+                                    )
+                                  : IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          isObscure = true;
+                                        });
+                                      },
+                                      icon: Icon(
+                                        Icons.remove_red_eye,
+                                        color: Colors.blue,
+                                      ),
                                     ),
-                                  )
-                                : IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        isObscure = true;
-                                      });
-                                    },
-                                    icon: Icon(
-                                      Icons.remove_red_eye,color: Colors.blue,
-                                    ),
-                                  ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: SizedBox(),
-                    ),
-                    Container(
-                      width: 300,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            MyColors.mainColor,
+                            ],
                           ),
                         ),
-                        onPressed: () async {
-                          setState(() {
-                            _saving = true;
-                          });
-                          try {
-                            final newUser =
-                                await _auth.createUserWithEmailAndPassword(
-                                    email: email, password: password);
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => SignInScreen(),
-                              ),
-                            );
-                            setState(() {
-                              _saving = false;
-                            });
-                          } catch (e) {
-                            print("$e");
-                          }
-                        },
-                        child: Text("إنشاء حساب جديد",style: TextStyle(
-
-                            fontSize:25,),),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        child: SizedBox(),
+                      ),
+                      Container(
+                        width: 300,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                              MyColors.mainColor,
+                            ),
+                          ),
+                          onPressed: () async {
+                            if (formKey.currentState!.validate()) {
+                              setState(() {
+                                _saving = true;
+                              });
+                              try {
+                                final newUser =
+                                    await _auth.createUserWithEmailAndPassword(
+                                        email: email, password: password);
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => SignInScreen(),
+                                  ),
+                                );
+                                setState(() {
+                                  _saving = false;
+                                });
+                              } catch (e) {
+                                print("$e");
+                              }
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  backgroundColor: Colors.red,
+                                  content: Text("يجب تسجيل الصنف"),
+                                ),
+                              );
+                            }
+                          },
+                          child: Text(
+                            "إنشاء حساب جديد",
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/screens/sign_in_chat_screen.dart
+++ lib/screens/sign_in_chat_screen.dart
@@ -19,145 +19,158 @@ class _SignInScreenState extends State<SignInScreen> {
   final _auth = FirebaseAuth.instance;
   late String email;
   late String password;
+  GlobalKey<FormState> formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        body: SafeArea(
-          child: ModalProgressHUD(
-            inAsyncCall: _saving,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Image.asset(
-                    //   "image/img_1.png",
-                    //   width: 200,
-                    //   height: 200,
-                    // ),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    Text("شركة الروضه",
-                      style: GoogleFonts.alexandria(
-                          color: Colors.blue[800],
-                          fontSize: 40
-                      ),),
-                    SizedBox(height: 20,),
-                    Container(
-                      width: 350,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.blue
-                        ),
+        body: Form(
+          key: formKey,
+          child: SafeArea(
+            child: ModalProgressHUD(
+              inAsyncCall: _saving,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Image.asset(
+                      //   "image/img_1.png",
+                      //   width: 200,
+                      //   height: 200,
+                      // ),
+                      SizedBox(
+                        height: 25,
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          onChanged: (value) {
-                            email = value;
-                          },
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "اكتب اسم الحساب",
+                      Text("شركة الروضه",
+                        style: GoogleFonts.alexandria(
+                            color: Colors.blue[800],
+                            fontSize: 40
+                        ),),
+                      SizedBox(height: 20,),
+                      Container(
+                        width: 350,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.blue
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            onChanged: (value) {
+                              email = value;
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "اكتب اسم الحساب",
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 25,
-                    ),
-                    Container(
-                      width: 350,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.blue
-                        ),
+                      SizedBox(
+                        height: 25,
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TextField(
-                                keyboardType: TextInputType.visiblePassword,
-                                obscureText: isObscure,
-                                onChanged: (value) {
-                                  password = value;
-                                },
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "كلمة المرور",
+                      Container(
+                        width: 350,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.blue
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  keyboardType: TextInputType.visiblePassword,
+                                  obscureText: isObscure,
+                                  onChanged: (value) {
+                                    password = value;
+                                  },
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "كلمة المرور",
+                                  ),
                                 ),
                               ),
-                            ),
-                            isObscure
-                                ? IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        isObscure = false;
-                                      });
-                                    },
-                                    icon: Icon(
-                                      Icons.visibility_off_outlined,color: Colors.blue
+                              isObscure
+                                  ? IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          isObscure = false;
+                                        });
+                                      },
+                                      icon: Icon(
+                                        Icons.visibility_off_outlined,color: Colors.blue
+                                      ),
+                                    )
+                                  : IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          isObscure = true;
+                                        });
+                                      },
+                                      icon: Icon(
+                                        Icons.remove_red_eye,color: Colors.blue
+                                      ),
                                     ),
-                                  )
-                                : IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        isObscure = true;
-                                      });
-                                    },
-                                    icon: Icon(
-                                      Icons.remove_red_eye,color: Colors.blue
-                                    ),
-                                  ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: SizedBox(
-                      ),
-                    ),
-                    Container(
-                      width: 300,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            MyColors.mainColor,
+                            ],
                           ),
                         ),
-                        onPressed: () async {
-                          setState(() {
-                            _saving = true;
-                          });
-                          try {
-                            await _auth.signInWithEmailAndPassword(
-                                email: email, password: password);
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => ListOfProducts(),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                        ),
+                      ),
+                      Container(
+                        width: 300,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                              MyColors.mainColor,
+                            ),
+                          ),
+                          onPressed: () async {
+                            if (formKey.currentState!.validate()) {
+                              setState(() {
+                                _saving = true;
+                              });
+                              try {
+                                await _auth.signInWithEmailAndPassword(
+                                    email: email, password: password);
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => ListOfProducts(),
+                                    ),
+                                  );
+                                setState(() {
+                                  _saving = false;
+                                });
+                              } catch (e) {
+                                print(e);
+                              }
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  backgroundColor: Colors.red,
+                                  content: Text("يجب تسجيل الصنف"),
                                 ),
                               );
-                            setState(() {
-                              _saving = false;
-                            });
-                          } catch (e) {
-                            print(e);
-                          }
-                        },
-                        child: Text("تسجيل الدخول",style: TextStyle(
+                            }
+                          },
+                          child: Text("تسجيل الدخول",style: TextStyle(
 
-                            fontSize:25),),
+                              fontSize:25),),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
