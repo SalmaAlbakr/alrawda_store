addfe9d5ed4cea5d2194b7629552602a9eb43611
SalmaAlbakr <salalbakr19988@gmail.com>
2023-09-20 23:56:46 +0300

    category

--- lib/controller/url_functions.dart
+++ lib/controller/url_functions.dart
@@ -4,9 +4,9 @@ import 'package:url_launcher/url_launcher.dart';
 //facebook function
 facebookURL() async {
   final Uri url = await Uri.parse(
-      'https://www.facebook.com/Rawdakabel?mibextid=ZbWKwL');
+      'https://www.facebook.com/Rawdakabel' );
   if (!await launchUrl(url
- ,
+ ,mode: LaunchMode.externalApplication,
     webViewConfiguration: WebViewConfiguration(
       enableJavaScript: false,
     )
